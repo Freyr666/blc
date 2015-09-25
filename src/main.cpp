896b3b04f376277c835a68e5a2bc7c1e20f3d8f4
freyr <freyr@debian.org>
2015-09-25 13:05:26 +0300

    Naive functor implemented

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include <algorithm>
 #include "Matrix.hpp"
 #include "Pic2Mat.hpp"
-//#include "naive.hpp"
 #include "./Naive/Naive.hpp"
 
+//timestamps for testing
+#include <sys/time.h>
+//
 
 int
 main(int argc, char **argv){
@@ -13,16 +15,15 @@ main(int argc, char **argv){
     std::cout << "Usage: prog ./image\n";
     return 0;
   }
-  
   Matrix<int>* blc = Pic2Mat<int>(argv[1]);
-
   Naive<int>* f = new Naive<int> (blc->get_cols_num(), blc->get_rows_num());
-  //auto f =  get_naive_alg<int>(blc->get_cols_num(), blc->get_rows_num());
-  //double rv = *(double*)blc->apply(Naive<int>(blc->get_cols_num(), blc->get_rows_num()));
+  struct timeval tvb, tva;
+  gettimeofday(&tvb,NULL);
   double rv = *(double*)f->eval(*(blc->val()));
+  gettimeofday(&tva,NULL);
   delete f;
   std::cout << "Fun f is ok\n";
   std::cout << rv << "\n";
-  
+  std::cout << "In " << tva.tv_usec - tvb.tv_usec << " us\n";
   return 0;
 }
